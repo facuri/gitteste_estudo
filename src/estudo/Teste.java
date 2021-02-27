ef62250a0e1efe512fef9228baa5e04d3c05df92
dalmo <mffacuri@terra.com.br>
2021-02-27 19:42:38 -0300

    quarto commit feature_estudo

--- src/estudo/Teste.java
+++ src/estudo/Teste.java
@@ -4,29 +4,32 @@ import java.util.Scanner;
 
 public class Teste {
    
+	
+	
+	String nome;
+	int idade;
+	
+	public void mostrar(){
+		System.out.println("Nome: " + nome + 
+				           "\nIdade: " + idade);
+		
+	}
+
 	public static void main(String[] args) {
 		  
-		   Scanner ent = new Scanner(System.in);
-		   String nome, mostrar; 
-		   int idade;
-		   mostrar = "";
+		 Teste t = new Teste();
+		 
+		 t.nome = "Marcos";
+		 t.idade = 12;
+		 t.mostrar();
 		   
-		    for(int i = 1; i <= 2; i++){
-			   System.out.print("Digite seu nome: ");
-			   nome = ent.next();
-			   System.out.print("Digite sua idade de " + nome + ": ");
-			   idade = ent.nextInt();
-			   
-			   mostrar += "\nNome: " + nome + 
-					      "\nIdade: " + idade;  
-			   
 			
 		}
-		    System.out.println(mostrar);
+		    
 		    
 		
 		 
 
-	}
+	 
 
 }
