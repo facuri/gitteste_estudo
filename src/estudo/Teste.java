ca5518f1d7e7e7ee823ad44907fddfb51af56129
dalmo <mffacuri@terra.com.br>
2021-02-27 19:29:45 -0300

    terceiro commit feature_estudo

--- src/estudo/Teste.java
+++ src/estudo/Teste.java
@@ -1,15 +1,30 @@
 package estudo;
 
+import java.util.Scanner;
+
 public class Teste {
-
+   
 	public static void main(String[] args) {
-
-         //Começando a commitar
-		//System.out.println("Hello Git Hub");
-		for(int i = 1; i <= 2; i++){
-			System.out.println("Hello Git Hub!!");
+		  
+		   Scanner ent = new Scanner(System.in);
+		   String nome, mostrar; 
+		   int idade;
+		   mostrar = "";
+		   
+		    for(int i = 1; i <= 2; i++){
+			   System.out.print("Digite seu nome: ");
+			   nome = ent.next();
+			   System.out.print("Digite sua idade de " + nome + ": ");
+			   idade = ent.nextInt();
+			   
+			   mostrar += "\nNome: " + nome + 
+					      "\nIdade: " + idade;  
+			   
 			
 		}
+		    System.out.println(mostrar);
+		    
+		
 		 
 
 	}
